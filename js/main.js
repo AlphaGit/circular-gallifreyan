dbe287e63d20926e79d27538bda7c4824c25d654
Alpha <alphagma@gmail.com>
2015-10-04 17:45:15 -0500

    Added requireJS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-window.CircularGallifreyanApp = window.CircularGallifreyanApp || function(userOptions) {
-	var defaultOptions = {
-	};
+define(['tokenizer'], function(tokenizer) {
+  var $text = $('#userText');
+  $('#doTranslate').click(redraw);
+  var $translatedBox = $('#translated');
+  var paper = Raphael('translated', $translatedBox.width(), $translatedBox.height());
 
-	
-};
+  function redraw() {
+    paper.clear();
+
+    var textToDraw = $text.val();
+    var drawSpecification = getDrawingSpecification(textToDraw);
+
+    var circle = paper.circle(pos.x, pos.y, 10);
+    circle.attr('fill', '#fff');
+    circle.attr('stroke', '#000');
+  }
+
+  function drawSpecification(text) {
+    var tokens = tokenizer(text);
+  }
+
+  function tokenizeText(text) {
+
+  }
+});
