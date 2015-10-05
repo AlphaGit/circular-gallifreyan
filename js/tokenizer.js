66b80c297a1a770a1545e96437a9cf9ba4f28747
Alpha <alphagma@gmail.com>
2015-10-05 06:13:42 -0500

    Parsing words

--- js/tokenizer.js
+++ js/tokenizer.js
@@ -1,7 +1,50 @@
 define('tokenizer', function() {
   return function(text) {
-    return text
-      .split(' ')
-      .map(function(word) { return word.split(''); });
+    return new InternalTokenizer(text)
+      .transformToLowercase()
+      .removeUnsupportedPunctuation()
+      .replaceSoftCs()
+      .separateInWords()
+      .separateInLetters()
+      .getResult();
+  };
+
+  function InternalTokenizer(initialText) {
+    var result = initialText;
+
+    this.transformToLowercase = function transformToLowercase() {
+      result = result.toLowerCase();
+      return this;
+    }
+
+    this.removeUnsupportedPunctuation = function removeUnsupportedPunctuation() {
+      result = result.replace(/[^\w\d\.\?!"'\-,\s]/g, '');
+      return this;
+    }
+
+    this.replaceSoftCs = function replaceSoftCs() {
+      result = result.replace(/c([ei])/g, 's$1');
+      return this;
+    };
+
+    this.separateInWords = function separateInWords() {
+      result = result.split(' ');
+      return this;
+    }
+
+    this.separateInLetters = function separateInLetters() {
+      var LETTER_SEPARATOR = /(ch|sh|th|ng|qu|.)/g;
+      result = result.map(function(word) {
+        var letters = [];
+        while ((match = LETTER_SEPARATOR.exec(word)) !== null)
+          letters.push(match[0]);
+        return letters;
+      });
+      return this;
+    }
+
+    this.getResult = function getResult() {
+      return result;
+    }
   }
 });
--- specs/tokenizerSpec.js
+++ specs/tokenizerSpec.js
@@ -8,11 +8,11 @@ describe("Tokenizer", function() {
     });
   });
 
-  it('should return a function', function() {
+  it('returns a function', function() {
     expect(typeof tokenizer === 'function').toBe(true);
   });
 
-  it('should return an array of words and letters in them', function() {
+  it('returns an array of words and letters in them', function() {
     var result = tokenizer('heres a few words');
 
     expect(result).toEqual([
@@ -21,5 +21,84 @@ describe("Tokenizer", function() {
       ['f', 'e', 'w'],
       ['w', 'o', 'r', 'd', 's']
     ]);
+  });
+
+  it('replaces /ce/ and /ci/ by /se/ and /si/ (soft c sound)', function() {
+    var result = tokenizer('aceciu');
+    expect(result).toEqual([ ['a', 's', 'e', 's', 'i', 'u'] ]);
+  });
+
+  it('returns all results in lowercase', function() {
+    expect(tokenizer('AEi')).toEqual([ ['a', 'e', 'i' ] ]);
+  });
+
+  it('treats ch as a single letter', function() {
+    expect(tokenizer('chiropractic')).toEqual([ ['ch', 'i', 'r', 'o', 'p', 'r', 'a', 'c', 't', 'i', 'c'] ]);
+  });
+
+  it('treats sh as a single letter', function() {
+    expect(tokenizer('shush smol sister')).toEqual([
+      ['sh', 'u', 'sh'],
+      ['s', 'm', 'o', 'l'],
+      ['s', 'i', 's', 't', 'e', 'r']
+    ]);
+  });
+
+  it('treats th as a single letter', function() {
+    expect(tokenizer('thanks totem')).toEqual([
+      ['th', 'a', 'n', 'k', 's'],
+      ['t', 'o', 't', 'e', 'm']
+    ]);
+  });
+
+  it('treats ng as a single letter', function() {
+    expect(tokenizer('ngsomethingn')).toEqual([ ['ng', 's', 'o', 'm', 'e', 'th', 'i', 'ng', 'n'] ]);
+  });
+
+  it('treats qu as a single letter', function() {
+    expect(tokenizer('quien es el doctor')).toEqual([
+      ['qu', 'i', 'e', 'n'],
+      ['e', 's'],
+      ['e', 'l'],
+      ['d', 'o', 'c', 't', 'o', 'r']
+    ]);
+  });
+
+  describe('punctuation', function() {
+    it('allows .', function() {
+      expect(tokenizer('a.b')).toEqual([ ['a', '.', 'b' ]]);
+    });
+
+    it('allows ?', function() {
+      expect(tokenizer('a?b')).toEqual([ ['a', '?', 'b' ]]);
+    });
+
+    it('allows !', function() {
+      expect(tokenizer('a!b')).toEqual([ ['a', '!', 'b' ]]);
+    });
+
+    it('allows "', function() {
+      expect(tokenizer('a"b')).toEqual([ ['a', '"', 'b' ]]);
+    });
+
+    it('allows \'', function() {
+      expect(tokenizer('a\'b')).toEqual([ ['a', '\'', 'b' ]]);
+    });
+
+    it('allows -', function() {
+      expect(tokenizer('a-b')).toEqual([ ['a', '-', 'b' ]]);
+    });
+
+    it('allows ,', function() {
+      expect(tokenizer('a,b')).toEqual([ ['a', ',', 'b' ]]);
+    });
+
+    it('allows digits', function() {
+      expect(tokenizer('R2D2-1')).toEqual([ ['r', '2', 'd', '2', '-', '1'] ]);
+    });
+
+    it('removes any other punctuation', function() {
+      expect(tokenizer('a$b')).toEqual([ ['a', 'b'] ]);
+    });
   });
 });
