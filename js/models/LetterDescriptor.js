d41a7b230c83959be258ba536c64a01e4de6a29e
Alpha <alphagma@gmail.com>
2015-10-21 19:54:13 -0500

    LetterDescriptor: baseline positions

--- js/models/LetterDescriptor.js
+++ js/models/LetterDescriptor.js
@@ -1,5 +1,72 @@
 define(function() {
-  return function LetterDescriptor(letter) {
+  var LetterDescriptor = function LetterDescriptor(letter) {
     this.letter = (letter || '').toLowerCase();
+    this.baselinePosition = getBaselinePosition(this.letter);
+    this.shape = getLetterShape(this.letter);
+    this.decorationType = getLetterDecorationType(this.letter);
+    this.decorationCount = getLetterDecorationCount(this.letter);
   };
+
+  LetterDescriptor.BASELINE_POSITIONS = {
+    ABOVE_BASELINE: 'ABOVE_BASELINE',
+    BELOW_BASELINE: 'BELOW_BASELINE',
+    ON_BASELINE: 'ON_BASELINE'
+  };
+
+  var _letterBaselinePositions = {
+    a: LetterDescriptor.BASELINE_POSITIONS.BELOW_BASELINE,
+    b: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    ch: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    d: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    e: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    f: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    g: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    h: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    i: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    j: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    k: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    l: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    m: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    n: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    o: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    p: LetterDescriptor.BASELINE_POSITIONS.ABOVE_BASELINE,
+    qu: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    r: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    sh: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    s: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    t: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    u: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    w: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    x: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    y: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    z: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    ng: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE,
+    th: LetterDescriptor.BASELINE_POSITIONS.ON_BASELINE
+  };
+
+  LetterDescriptor.SHAPES = {
+    SMALL_CIRCLE: 'SMALL_CIRCLE'
+  };
+
+  LetterDescriptor.DECORATION_TYPES = {
+    NONE: 'NONE'
+  };
+
+  function getBaselinePosition(letter) {
+    return _letterBaselinePositions[letter];
+  };
+
+  function getLetterShape(letter) {
+    return LetterDescriptor.SHAPES.SMALL_CIRCLE;
+  };
+
+  function getLetterDecorationType(letter) {
+    return LetterDescriptor.DECORATION_TYPES.NONE;
+  }
+
+  function getLetterDecorationCount(letter) {
+    return 0;
+  }
+
+  return LetterDescriptor;
 });
--- specs/models/LetterDescriptorSpec.js
+++ specs/models/LetterDescriptorSpec.js
@@ -17,13 +17,75 @@ describe('LetterDescriptor', function() {
     expect(newObject).toBeDefined();
   });
 
-  it('will store the letter passed to it', function() {
+  it('stores the letter passed to it', function() {
     var newObject = new LetterDescriptor('a');
     expect(newObject.letter).toBe('a');
   });
 
-  it('will store the lowercase representation of the letter passed to it', function() {
+  it('stores the lowercase representation of the letter passed to it', function() {
     var newObject = new LetterDescriptor('A');
     expect(newObject.letter).toBe('a');
+  });
+
+  it('identifies the baseline position of the letter', function() {
+    var newObject = new LetterDescriptor('a');
+    expect(newObject.baselinePosition).toBe(LetterDescriptor.BASELINE_POSITIONS.BELOW_BASELINE);
+  });
+
+  it('identifies the shape of the letter', function() {
+    var newObject = new LetterDescriptor('a');
+    expect(newObject.shape).toBe(LetterDescriptor.SHAPES.SMALL_CIRCLE);
+  });
+
+  it('identifies the decoration type of the letter', function() {
+    var newObject = new LetterDescriptor('a');
+    expect(newObject.decorationType).toBe(LetterDescriptor.DECORATION_TYPES.NONE);
+  });
+
+  it('identifies the number of decorations of the letter', function() {
+    var newObject = new LetterDescriptor('a');
+    expect(newObject.decorationCount).toBe(0);
+  });
+
+  describe('letter baseline positions', function() {
+    var letterExpectations = [
+      { letter: 'a', baseLinePositionIdentifier: 'BELOW_BASELINE' },
+      { letter: 'b', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'ch', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'd', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'e', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'f', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'g', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'h', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'i', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'j', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'k', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'l', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'm', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'n', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'o', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'p', baseLinePositionIdentifier: 'ABOVE_BASELINE' },
+      { letter: 'qu', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'r', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'sh', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 's', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 't', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'u', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'v', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'w', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'x', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'y', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'z', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'ng', baseLinePositionIdentifier: 'ON_BASELINE' },
+      { letter: 'th', baseLinePositionIdentifier: 'ON_BASELINE' }
+    ];
+
+    for (var index in letterExpectations) {
+      var le = letterExpectations[index];
+      it('identifies ' + le.letter  +'\'s baseline position as ' + le.baseLinePositionIdentifier, function() {
+        var letter = new LetterDescriptor(le.letter);
+        expect(letter.baselinePosition).toBe(LetterDescriptor.BASELINE_POSITIONS[le.baseLinePositionIdentifier]);
+      });
+    }
   });
 });
